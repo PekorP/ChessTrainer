13536c5d1c255dd64f099aa9ceb298499274a664
Pekor <pavel.chistyakov.job@gmail.com>
2022-02-02 16:11:42 +0300

    Отображение координат рядом с доской

--- ChessTrainer/ViewModels/CoordinatesViewModel.cs
+++ ChessTrainer/ViewModels/CoordinatesViewModel.cs
@@ -9,11 +9,15 @@ namespace ChessTrainer.ViewModels
 {
     class CoordinatesViewModel : BaseViewModel
     {
+        public List<int> Ranks { get; set; }
+        public List<char> Files { get; set; }
         public Board Board{ get; set; }
 
         public CoordinatesViewModel()
         {
             Board = new Board();
+            Ranks = new List<int> { 8, 7, 6, 5, 4, 3, 2, 1 };
+            Files = new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
         }
     }
 }
