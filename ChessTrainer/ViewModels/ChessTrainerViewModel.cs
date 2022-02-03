109f13ea41e1dcc9d24fa59b9a5d4190993b3df2
Pekor <pavel.chistyakov.job@gmail.com>
2022-02-03 16:06:15 +0300

    Реализация закрытия приложения

---
+++ ChessTrainer/ViewModels/ChessTrainerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ChessTrainer.Commands;
+using System.Windows;
+
+namespace ChessTrainer.ViewModels
+{
+    internal class ChessTrainerViewModel : BaseViewModel
+    {
+        private RelayCommand closeApplication;
+        public RelayCommand CloseApplication
+        {
+            get {return closeApplication ?? (closeApplication = new RelayCommand(obj =>
+                {
+                    Application.Current.MainWindow.Close();
+                }));
+                }
+        }
+        public ChessTrainerViewModel()
+        {
+        }
+    }
+}
