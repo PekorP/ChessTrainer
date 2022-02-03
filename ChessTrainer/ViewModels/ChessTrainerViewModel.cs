a087b1e93520df8489aaa9396e9112fb9bc4ecac
Pekor <pavel.chistyakov.job@gmail.com>
2022-02-03 16:16:47 +0300

    Реализация сворачивания приложения

--- ChessTrainer/ViewModels/ChessTrainerViewModel.cs
+++ ChessTrainer/ViewModels/ChessTrainerViewModel.cs
@@ -5,15 +5,39 @@ namespace ChessTrainer.ViewModels
 {
     internal class ChessTrainerViewModel : BaseViewModel
     {
+
+        #region Команды
+
+        #region Команда закрытия приложения
+
         private RelayCommand closeApplication;
         public RelayCommand CloseApplication
         {
-            get {return closeApplication ?? (closeApplication = new RelayCommand(obj =>
-                {
-                    Application.Current.MainWindow.Close();
-                }));
-                }
+            get { return closeApplication ?? (closeApplication = new RelayCommand(obj =>
+                 {
+                     Application.Current.MainWindow.Close();
+                 }));
+            }
         }
+
+        #endregion
+
+        #region Команда сворачивания приложения
+
+        private RelayCommand minimizeAppllicatiom;
+        public RelayCommand MinimizeAppllicatiom
+        {
+            get
+            {
+                return minimizeAppllicatiom ?? (minimizeAppllicatiom = new RelayCommand(obj =>
+                {
+                    Application.Current.MainWindow.WindowState = WindowState.Minimized;
+                }));
+            }
+        }
+
+        #endregion
+        #endregion
         public ChessTrainerViewModel()
         {
         }
