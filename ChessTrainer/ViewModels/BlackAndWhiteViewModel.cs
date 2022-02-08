c0b3190cda4005145e920042bd4a09b62e0e21cc
Pekor <pashachistjakov@gmail.com>
2022-02-08 21:17:31 +0300

    Реализация логики тренажера

--- ChessTrainer/ViewModels/BlackAndWhiteViewModel.cs
+++ ChessTrainer/ViewModels/BlackAndWhiteViewModel.cs
@@ -1,4 +1,5 @@
-﻿using ChessTrainer.Models;
+﻿using ChessTrainer.Commands;
+using ChessTrainer.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,39 @@ namespace ChessTrainer.ViewModels
 {
     class BlackAndWhiteViewModel : BaseViewModel
     {
+
+        #region Команды
+
+        #region Команда изменения цвета доски
+
+        private RelayCommand checkIsRightAnswer;
+        public RelayCommand CheckIsRightAnswer
+        {
+            get
+            {
+                return checkIsRightAnswer ??
+                  (checkIsRightAnswer = new RelayCommand(obj =>
+                  {
+                      switch (obj) {
+                          case "Black":
+                              Cell newCellBlack = new Cell() { Color = Enums.CellColor.Black, File = RandomCell.File, Rank = RandomCell.Rank };
+                              if (RandomCell.Equals(newCellBlack)) { CountRightAnswers++; IsRightAnswer = true; } else IsRightAnswer = false;
+                              break;
+                          case "White":
+                              Cell newCellWhite = new Cell() { Color = Enums.CellColor.White, File = RandomCell.File, Rank = RandomCell.Rank };
+                              if (RandomCell.Equals(newCellWhite)) { CountRightAnswers++; IsRightAnswer = true; } else IsRightAnswer = false;
+                              break;
+                      }
+                      TotalCountAnswers++;
+                      RandomCell = Board.Cells[new Random().Next(Board.Cells.Count())];
+                  }));
+            }
+        }
+
+        #endregion
+
+        #endregion
+
         public Board Board { get; set; }
 
         private Cell randomCell;
@@ -32,12 +66,22 @@ namespace ChessTrainer.ViewModels
             set { totalCountAnswers = value; OnPropertyChanged(); }
         }
 
+        private bool? isRightAnswer;
+
+        public bool? IsRightAnswer
+        {
+            get { return isRightAnswer; }
+            set { isRightAnswer = value; OnPropertyChanged(); }
+        }
+
+
         public BlackAndWhiteViewModel()
         {
             Board = new Board();
             RandomCell = Board.Cells[new Random().Next(Board.Cells.Count())];
             CountRightAnswers = 0;
             TotalCountAnswers = 0;
+            IsRightAnswer = null;
         }
     }
 }
