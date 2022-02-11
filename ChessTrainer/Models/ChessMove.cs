f2a3e8a60a50f946a8e2d91eddf7a99c0deeff46
Pekor <pavel.chistyakov.job@gmail.com>
2022-02-11 15:24:26 +0300

    Создание модели для шахматного хода

---
+++ ChessTrainer/Models/ChessMove.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTrainer.Models
+{
+    class ChessMove
+    {
+
+        //Для примера: { 1, "Pa2-a4", "Ng1-f3" };
+        public int NumberOfMove { get; set; }
+        public string WhiteMove { get; set; }
+        public string BlackMove { get; set; }
+
+        public ChessMove(int numberOfMove, string whiteMove, string blackMove)
+        {
+            NumberOfMove = numberOfMove;
+            WhiteMove = whiteMove;
+            BlackMove = blackMove;
+        }
+
+        public string MoveParser(string move)
+        {
+            return "";
+        }
+    }
+}
