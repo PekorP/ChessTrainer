0139c7ebf57bd4bf9c2cc64603c8782e18c6dfd4
Pekor <pavel.chistyakov.job@gmail.com>
2022-02-14 19:51:03 +0300

    Изменение стиля ListBox

--- ChessTrainer/ViewModels/GameVoiceActingViewModel.cs
+++ ChessTrainer/ViewModels/GameVoiceActingViewModel.cs
@@ -26,13 +26,6 @@ namespace ChessTrainer.ViewModels
         {
             AddedChessMove = new ChessMove();
             Moves = new ObservableCollection<ChessMove>();
-            Moves.Add(new ChessMove(1, "Pe2-e4", "Pe7-e5"));
-            Moves.Add(new ChessMove(2, "Ng1-f3", "Nb8-c6"));
-            Moves.Add(new ChessMove(3, "Bf1-c4", "Ng8-f6"));
-            Moves.Add(new ChessMove(4, "Bf1-c4", "Ng8-f6"));
-            Moves.Add(new ChessMove(5, "Bf1-c4", "Ng8-f6"));
-            Moves.Add(new ChessMove(6, "Bf1-c4", "Ng8-f6"));
-            Moves.Add(new ChessMove(7, "Bf1-c4", "Ng8-f6"));   
         }
     }
 }
