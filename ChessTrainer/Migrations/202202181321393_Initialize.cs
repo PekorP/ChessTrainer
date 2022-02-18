1d8ca8c604ff911fa1c69739c276ad4734cc7e0e
Pekor <pavel.chistyakov.job@gmail.com>
2022-02-18 16:26:04 +0300

    Создание миграции инициализации

---
+++ ChessTrainer/Migrations/202202181321393_Initialize.cs
@@ -0,0 +1,27 @@
+﻿namespace ChessTrainer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initialize : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.MaterialAdvantage",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Image = c.String(),
+                        Advantage = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.MaterialAdvantage");
+        }
+    }
+}
--- ChessTrainer/Models/EF/MaterialAdvantage.cs
+++ ChessTrainer/Models/EF/MaterialAdvantage.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ChessTrainer.Models.EF
 {
+    [Table("MaterialAdvantage")]
     public class MaterialAdvantage
     {
         public int ID { get; set; }
