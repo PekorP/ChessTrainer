5b16d58863a7241bb8d7e9084273a450ae729794
Pekor <pavel.chistyakov.job@gmail.com>
2022-02-18 15:39:05 +0300

    Создание модели и контекста EF для "Материального преимущества"

---
+++ ChessTrainer/Models/EF/ChessTrainerContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTrainer.Models.EF
+{
+    public class ChessTrainerContext : DbContext
+    {
+        public ChessTrainerContext() : base("ConnectionString")
+        {
+        }
+        public DbSet<MaterialAdvantage> MaterialAdvantages { get; set; }
+    }
+}
---
+++ ChessTrainer/Models/EF/MaterialAdvantage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTrainer.Models.EF
+{
+    public class MaterialAdvantage
+    {
+        public int ID { get; set; }
+        public string Image { get; set; }
+        public int Advantage { get; set; }
+    }
+}
