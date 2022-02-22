83537c10fc68c7a4d1a2feeaf121046e532ae568
Pekor <pavel.chistyakov.job@gmail.com>
2022-02-22 15:13:23 +0300

    Реализация класса Knight

---
+++ ChessTrainer/Models/Pieces/Knight.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTrainer.Models.Pieces
+{
+    class Knight : IPiece
+    {
+        public bool CanBeat(Cell CellFrom, Cell CellTo)
+        {
+            if (((CellFrom.File + 1) == CellTo.File) && ((CellFrom.Rank + 2) == CellTo.Rank)) return true;
+            if (((CellFrom.File + 2) == CellTo.File) && ((CellFrom.Rank + 1) == CellTo.Rank)) return true;
+
+            if (((CellFrom.File + 1) == CellTo.File) && ((CellFrom.Rank - 2) == CellTo.Rank)) return true;
+            if (((CellFrom.File + 2) == CellTo.File) && ((CellFrom.Rank - 1) == CellTo.Rank)) return true;
+
+            if (((CellFrom.File - 1) == CellTo.File) && ((CellFrom.Rank + 2) == CellTo.Rank)) return true;
+            if (((CellFrom.File - 2) == CellTo.File) && ((CellFrom.Rank + 1) == CellTo.Rank)) return true;
+
+            if (((CellFrom.File - 1) == CellTo.File) && ((CellFrom.Rank - 2) == CellTo.Rank)) return true;
+            if (((CellFrom.File - 2) == CellTo.File) && ((CellFrom.Rank - 1) == CellTo.Rank)) return true;
+
+            return false;
+        }
+    }
+}
