29ffcea8b9b57b8a8b74cead8d90be119f93bf7a
Pekor <pavel.chistyakov.job@gmail.com>
2022-02-22 15:52:05 +0300

    Реализация класса Bishop

---
+++ ChessTrainer/Models/Pieces/Bishop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTrainer.Models.Pieces
+{
+    class Bishop : IPiece
+    {
+        public bool CanBeat(Cell CellFrom, Cell CellTo)
+        {
+            for (int i = 1; i <= 8; i++) //Для движения влево вниз
+            {
+                if ((CellFrom.File - i == CellTo.File) && (CellFrom.Rank - i == CellTo.Rank)) return true;
+            }
+
+            for (int i = 1; i <= 8; i++) //Для движения влево вверх
+            {
+                if ((CellFrom.File - i == CellTo.File) && (CellFrom.Rank + i == CellTo.Rank)) return true;
+            }
+
+            for (int i = 1; i <= 8; i++) //Для движения вправо вниз
+            {
+                if ((CellFrom.File + i == CellTo.File) && (CellFrom.Rank - i == CellTo.Rank)) return true;
+            };
+
+            for (int i = 1; i <= 8; i++) //Для движения вправо вверх
+            {
+                if ((CellFrom.File + i == CellTo.File) && (CellFrom.Rank + i == CellTo.Rank)) return true;
+            }
+
+            return false;
+        }
+    }
+}
