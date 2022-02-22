8c3426c8fc1cdfe2823c545f776868cd22ebd3e1
Pekor <pavel.chistyakov.job@gmail.com>
2022-02-22 15:01:03 +0300

    Создание интерфейса IPiece и его реализация в классе Rook

---
+++ ChessTrainer/Models/Pieces/IPiece.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTrainer.Models.Pieces
+{
+    public interface IPiece
+    {
+        bool CanBeat(Cell CellFrom, Cell CellTo);
+    }
+}
---
+++ ChessTrainer/Models/Pieces/Rook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTrainer.Models.Pieces
+{
+    class Rook : IPiece
+    {
+        public bool CanBeat(Cell CellFrom, Cell CellTo)
+        {
+            return ((CellFrom.Rank == CellTo.Rank) || (CellFrom.File == CellTo.File));
+        }
+    }
+}
