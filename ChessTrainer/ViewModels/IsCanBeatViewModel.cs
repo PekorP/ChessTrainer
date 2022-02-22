f845b046d24155ecd019a21de622be07c8e58792
Pekor <pavel.chistyakov.job@gmail.com>
2022-02-22 17:41:56 +0300

    Привязка текста к свойствам

--- ChessTrainer/ViewModels/IsCanBeatViewModel.cs
+++ ChessTrainer/ViewModels/IsCanBeatViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using ChessTrainer.Models;
 using ChessTrainer.Models.Pieces;
 using System;
+using ChessTrainer.Enums;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,20 @@ namespace ChessTrainer.ViewModels
     {
         Board board = new Board();
         List<Piece> pieces = new List<Piece> { new Knight(), new Rook(), new Queen(), new Bishop() };
+        Dictionary<Pieces, Char> piecesNames = new Dictionary<Pieces, char>()
+        {
+            {Pieces.Rook, 'R' },
+            {Pieces.Queen, 'Q' },
+            {Pieces.Bishop, 'B' },
+            {Pieces.Knight, 'N' }
+        };
+
+        private char pieceName;
+        public char PieceName
+        {
+            get { return pieceName; }
+            set { pieceName = value; OnPropertyChanged(); }
+        }
 
         private Cell cellFrom;
         public Cell CellFrom
@@ -55,7 +70,9 @@ namespace ChessTrainer.ViewModels
                 CellFrom = board.Cells[new Random().Next(board.Cells.Count())];
                 CellTo = board.Cells[new Random().Next(board.Cells.Count())];
             } while (CellFrom == CellTo);
+
             CellFrom.Piece = pieces[new Random().Next(pieces.Count())];
+            PieceName = piecesNames[CellFrom.Piece.PieceType];
         }
 
     }
