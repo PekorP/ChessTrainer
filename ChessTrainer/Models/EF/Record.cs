2a0296672edfadee8033d09e34d466e37a392acb
Pekor <pavel.chistyakov.job@gmail.com>
2022-04-03 17:20:15 +0300

    Добавление новых классов на основе таблиц БД

---
+++ ChessTrainer/Models/EF/Record.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTrainer.Models.EF
+{
+    [Table("Records")]
+    public class Record
+    {
+        public int ID { get; set; }
+        public int IdUser { get; set; }
+        public int IdTrainer { get; set; }
+        public int Result { get; set; }
+    }
+}
---
+++ ChessTrainer/Models/EF/Trainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTrainer.Models.EF
+{
+    [Table("Trainers")]
+    public class Trainer
+    {
+        public int ID { get; set; }
+        public string TrainerName{ get; set; }
+    }
+}
